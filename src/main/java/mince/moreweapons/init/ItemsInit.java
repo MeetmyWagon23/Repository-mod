572f663f978a4af5c7434eada153c15adde7de5a
Wagon <95537914+meetmywagon23@users.noreply.github.com>
2023-01-16 22:01:18 -0500

    added a Thundering Enchantment; will not summon lightning bolt. (as intentional).

--- src/main/java/mince/moreweapons/init/ItemsInit.java
+++ src/main/java/mince/moreweapons/init/ItemsInit.java
@@ -1,4 +1,10 @@
 package mince.moreweapons.init;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+
+import java.rmi.registry.Registry;
+
 public class ItemsInit {
+
 }
