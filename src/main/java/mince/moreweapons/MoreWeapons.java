c1b145db4e85184944057222d86d45486dac0795
Wagon <95537914+MeetmyWagon23@users.noreply.github.com>
2023-01-16 22:01:18 -0500

    added a Thundering Enchantment; will not summon lightning bolt. (as intentional).

--- src/main/java/mince/moreweapons/MoreWeapons.java
+++ src/main/java/mince/moreweapons/MoreWeapons.java
@@ -1,5 +1,6 @@
 package mince.moreweapons;
 
+import mince.moreweapons.enchantment.EnchantRegistry;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,8 @@ public class MoreWeapons implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "moreweapons";
+
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
 	@Override
@@ -17,5 +20,6 @@ public class MoreWeapons implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		EnchantRegistry.init();
 	}
 }
